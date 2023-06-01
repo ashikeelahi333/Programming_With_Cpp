81276c4acefc1f7b9fc983761343ed795c7becb3
ashikeelahi3 <ashikeelahi3@gmail.com>
2023-06-01 21:10:21 +0600

    Letrure 01

---
+++ 01_Simple_Calculator.cpp
@@ -0,0 +1,27 @@
+// Calculator Program
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int a, b;
+	
+	cout << "Input a number =>";
+	cin >> a;
+	cout << "Input another number =>";
+	cin >> b;
+	
+	cout <<a<<" + "<<b<<" = " << a+b << endl;
+	cout <<a<<" - "<<b<<" = " << a-b << endl;
+	cout <<a<<" * "<<b<<" = " << a*b << endl;
+	cout <<a<<" / "<<b<<" = " << a/b << endl;
+	cout <<a<<" % "<<b<<" = " << a%b << endl;
+}
+
+
+//	std::cout << "6+4 = "; // string -> "jdd"
+//	std::cout << 6+4;
+//	cout << "6 + 4 = " << 6+4;
+
+
+
